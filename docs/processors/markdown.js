7ec3c152f15d50cb7a884f793065e390f81d7850
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2015-03-14 17:03:14 +0100

    feat(dgeni): debug markdown

--- docs/processors/markdown.js
+++ docs/processors/markdown.js
@@ -50,11 +50,12 @@ module.exports = function renderMarkdownProcessor() {
     $runBefore: ['parsing-tags'],
     $process: function(docs) {
       return Q.all(docs.map(function(doc) {
-        console.log('Processing: ' + doc);
+        console.log('Processing: ' + getTitle(doc.fileInfo.content));
         if (doc.docType !== 'markdownFile') {
           return doc;
         }
         return Q.nfcall(markdownize, doc.fileInfo.content).then(function (rendered) {
+          console.log('Processed: ' + getTitle(doc.fileInfo.content));
           return {
             fileInfo: doc.fileInfo,
             name: getTitle(doc.fileInfo.content),
