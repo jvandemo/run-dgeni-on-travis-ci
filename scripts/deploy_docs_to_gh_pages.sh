9c2c3837eec64918c1ae5c1ae7cf69cb70fb17ad
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2015-03-14 18:06:53 +0100

    feat(scripts): update deploy script

--- scripts/deploy_docs_to_gh_pages.sh
+++ scripts/deploy_docs_to_gh_pages.sh
@@ -30,8 +30,7 @@ git config user.email "travis@angular.io" || exit 1
 
 git add -A . || exit 1
 git commit --allow-empty -m "Regenerated documentation for $CURRENT_COMMIT" || exit 1
-# git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages > /dev/null 2>&1
-git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages
+git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages > /dev/null 2>&1
 
 echo "Cleaning up temp files"
 rm -Rf $TEMP_DIRECTORY
