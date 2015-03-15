996000edb1cd961d111004b0455bf3a22134aaca
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2015-03-15 18:17:39 +0100

    feat(scripts): update deploy script

--- scripts/deploy_docs_to_gh_pages.sh
+++ scripts/deploy_docs_to_gh_pages.sh
@@ -5,6 +5,12 @@ echo "Target: gh-pages branch"
 TEMP_DIRECTORY="/tmp/__temp_static_content"
 CURRENT_COMMIT=`git rev-parse HEAD`
 # ORIGIN_URL=`git config --get remote.origin.url`
+COMMIT_MESSAGE=`git log --format=%B --no-merges -n 1`
+
+if [ !("COMMIT_MESSAGE" | grep "\[build-docs\]") ]; then
+  exit 1
+fi
+
 ORIGIN_URL="https://github.com/jvandemo/run-dgeni-on-travis-ci.git"
 ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
 
